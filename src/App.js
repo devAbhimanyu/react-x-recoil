e920282782be24a71b485ce37f3dc1b13fee3bc2
devabhimanyu <dev.chauhan.singh.abhimanyu@gmail.com>
2021-08-14 22:07:32 +0530

    chore: base sol

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import { RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,} from 'recoil'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Recoil
+    </div>
+  );
+}
+
+export default App;
